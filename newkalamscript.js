5fa23ddf745cd8ba5088224cc9b733e17efd2df1
Ali <leaflits@gmail.com>
2022-12-28 22:29:14 +0200

    first site

--- newkalamscript.js
+++ newkalamscript.js
@@ -107,7 +107,6 @@ let _fmain = parent.fmain,
             joiningPplClass = _fmain.document.getElementsByClassName("main-nickg");
             listTarget = _fwindowlist.document.getElementById("windowlist");
             input = parent.fform.document.querySelector("#say");
-            input.dir = "rtl";
             listObserver.observe(listTarget, objConfig);
             mainObserver.observe(mainTarget, objConfig);
             fform.onkeydown = keysHandelr;
