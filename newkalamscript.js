d93117e15b94aaf27b2de0087c3042b464583192
Ali <leaflits@gmail.com>
2023-01-08 14:41:22 +0200

    first site

--- newkalamscript.js
+++ newkalamscript.js
@@ -48,7 +48,7 @@ let _fmain = parent.fmain,
                 setTimeout(s => {
                     _fwindowlist.sendcmd_real("say", message1, s);
                     _fwindowlist.sendcmd(`/winclose ${s}`);
-                },4000, join)
+                },6000, join)
                 personsGotMyMsg1.add(join);
                 let li = document.createElement("li");
                 li.innerText = join
