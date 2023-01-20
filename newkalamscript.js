be172d54bb012f1b31b30857b7d757399b8774b4
Ali <leaflits@gmail.com>
2023-01-20 20:51:42 +0200

    first site

--- newkalamscript.js
+++ newkalamscript.js
@@ -113,6 +113,7 @@ let _fmain = parent.fmain,
                                 _fwindowlist.sendcmd(`/query ${this.innerText}`);
                             }
                             ol1.append(li);
+                            li.style.transform = "rotate(20deg)";
                             setTimeout(_ => { li.scrollIntoView() }, 1000);
                             if (femalesNames.has(zozo.at(-1))) {
                                 input.placeholder = `${zozo.at(-1)} inSet`;
