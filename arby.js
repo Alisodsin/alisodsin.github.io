1b1efc803cbd450d28328cb53dba29bc91ec2e67
Ali <leaflits@gmail.com>
2023-04-18 01:33:19 +0200

    first site

--- arby.js
+++ arby.js
@@ -656,7 +656,7 @@ button12.onclick = _ => {
 button13.innerText = "gdMsg"
 button13.onclick = _ => {
     if (msgSwitch == 0) {
-        msg1 = "تسمعى شخرتى وانا بفشخك نيك يا لبوه؟"
+        msg1 = "انيكك صوت بعنف"
         msg2 = "عندك جيتسى او تلجرام؟"
         button13.innerText = "bdMsg"
         msgSwitch = 1
