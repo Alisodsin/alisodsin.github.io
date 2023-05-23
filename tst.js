3581c4b30a3aac6af2c9d7425a10f2404cf967eb
Ali <leaflits@gmail.com>
2023-05-24 00:59:34 +0300

    first site

--- tst.js
+++ tst.js
@@ -621,7 +621,7 @@ function buttonsCreator() {
                 button.innerText = "S";
                 button.onclick = function () {
                     if (this.innerText == "S") {
-                        containersDiv.style.width = "90%";
+                        containersDiv.style.width = "70%";
                         this.innerText = "L"
                     }
                     else {
