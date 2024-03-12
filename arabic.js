6441130d58e7b83297b412a20bfa6f807364cddc
Alisodsin <leaflits@gmail.com>
2024-03-12 08:46:28 +0200

    first site

--- arabic.js
+++ arabic.js
@@ -6,7 +6,7 @@ let targetElement = document.getElementById('notify_private');
 let observerr = new MutationObserver(privo);
 let msg = (new Date().getHours() >= 2 && new Date().getHours() <= 14) ? "صباح الخير" : "مساء الخير"
 let imsg = msg;
-let observer = new MutationObserver(msgSend);
+let observer = new MutationObserver(doit);
 let fms = document.getElementsByClassName("username bcolor23");
 let list = document.createElement("ol");
 let button = document.createElement("button");
@@ -20,56 +20,81 @@ let females = new Set();
 let namesSource = document.createElement("ol");
 let framo = document.createElement("iframe");
 let parentDiv = document.createElement("div");
-let fmlgiturl = `https://raw.githubusercontent.com/Alisodsin/alisodsin.github.io/main/femaleNames.json`;
-let mlsgiturl = 'https://raw.githubusercontent.com/Alisodsin/alisodsin.github.io/main/males.json';
+let zozo = new Map();
+let testFset = new Set();
+let privt = $("#private_box")[0];
+let closo = document.getElementById("private_close")
 framo.src = "https://99f2537e-72f2-4e73-9898-cc9c6e98f207-00-aoo727xiohcd.janeway.replit.dev/";
 parentDiv.id = "conto";
+
+function sendMsg(idd, msgg) {
+    return fetch("system/private_process.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: `target=${idd}&content=${msgg}&token=${utk}`
+    })
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-function msgSend() {
-
+async function doit() {
     let lastF = fms[fms.length - 1],
         name = lastF.innerText,
         id = lastF.getAttribute("data-uid");
-
     if (!messagedFs.has(id) && lastF.parentElement.innerText.includes("زائر") && checkForFemaleName(name, females)) {
         messagedFs.add(id);
-        fetch("system/private_process.php", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-            },
-            body: `target=${id}&content=${msg}&token=${utk}`
-        }).then(_ => {
-            console.log(name);
-            let li = document.createElement("li");
-            li.innerText = name;
-            list.appendChild(li);
-            li.style.width = "fit-content"
-            li.scrollIntoView();
-        }
-        )
-    }
-    else if (!messagedMs.has(id) && !messagedFs.has(id) && lastF.parentElement.innerText.includes("زائر")) {
-        messagedMs.add(id)
-        let li = document.createElement("li")
+        await sendMsg(id, msg);
+        console.log(name);
+        let li = document.createElement("li");
         li.innerText = name;
+        list.appendChild(li);
         li.style.width = "fit-content"
-        namesSource.append(li)
-        li.scrollIntoView()
+        li.scrollIntoView();
+    }
+    else if (!messagedMs.has(id) && !messagedFs.has(id) && !zozo.has(name) && lastF.parentElement.innerText.includes("زائر")) {
+        if (checkForFemaleName(name, testFset)) {
+            zozo.set(name, id)
+            console.log(zozo);
+        }
+        else {
+            messagedMs.add(id)
+            let li = document.createElement("li")
+            li.innerText = name;
+            li.style.width = "fit-content"
+            namesSource.append(li)
+            li.scrollIntoView()
+        }
     }
 }
 async function privo() {
-    oh();
-    getPrivate();
-    await sleep(2000);
-    msgList[0].click();
-}
-observer.isConnectd = false;
-observerr.isConnectd = false;
+    if (privt.style.display == "none") {
+        oh();
+        $.post('system/box/private_notify.php', {
+            token: utk,
+        }, function (response) {
+            let ele = (new DOMParser()).parseFromString(response, "text/html").body.querySelector(".ulist_container").children[0].children[1]
+            openPrivate(ele.getAttribute("data"), ele.getAttribute("value"))
+            showPrivateAd()
+            privReload = 1
+            lastPriv = 0
+            chat_reload(true)
+        });
+    }
+    else {
+        closo.click();
+        await sleep(1000)
+        privo();
+    }
 
+}
+closo.onclick = _ => {
+    no();
+}
+
+privt.style.display = "none"
 buttonsContainer.id = "buttonsContainer"
 
 list.style.backgroundColor = "black"
@@ -126,19 +151,13 @@ button0.onclick = function () {
 }
 button.onclick = function () {
 
-    if (observer.isConnectd && observerr.isConnectd) {
-        observer.disconnect()
-        observer.isConnectd = false
-        observerr.disconnect()
-        observerr.isConnectd = false
+    if (females.size) {
+        females.clear()
         this.style.backgroundColor = "red";
         this.innerText = "F"
     }
     else {
-        observer.observe(elTarget, { childList: true, subtree: true, attributes: false, characterData: false });
-        observer.isConnectd = true;
-        observerr.observe(targetElement, { attributes: true });
-        observerr.isConnectd = true;
+        females = structuredClone(testFset)
         button.style.backgroundColor = "green"
         this.innerText = "N"
 
@@ -230,31 +249,17 @@ document.head.append(style);
 
 
 function oh() {
-    if (observer.isConnectd && observerr.isConnectd) {
+    if (females.size) {
         button.click();
 
     }
 
 }
 function no() {
-    if (!(observer.isConnectd && observerr.isConnectd)) {
+    if (!(females.size)) {
         button.click();
     }
 }
-$('.chat_head').on('click', function () {
-    if (button1.innerText == "S") {
-        button1.click();
-    }
-
-    oh()
-});
-$('.fa.fa-times').on('click', function () {
-
-    if (button1.innerText == "H") {
-        button1.click();
-    }
-    no()
-});
 function checkForFemaleName(str, set) {
     if (str.includes("|")) {
         return false
@@ -300,44 +305,42 @@ function checkForFemaleName(str, set) {
         let stro = str.replaceAll("ة", "ة ")
         words = stro.split(/\s/);
         for (const word of words) {
-          if (set.has(word.toLowerCase())) {
-            return true;
-          }
+            if (set.has(word.toLowerCase())) {
+                return true;
+            }
         }
-      }
-      if (str.includes("ء")) {
+    }
+    if (str.includes("ء")) {
         let stro = str.replaceAll("ء", "ء ")
         words = stro.split(/\s/);
         for (const word of words) {
-          if (set.has(word.toLowerCase())) {
-            return true;
-          }
+            if (set.has(word.toLowerCase())) {
+                return true;
+            }
         }
-      }
-      if (str.includes("د")) {
+    }
+    if (str.includes("د")) {
         let stro = str.replaceAll("د", "د ")
         words = stro.split(/\s/);
         for (const word of words) {
-          if (set.has(word.toLowerCase())) {
-            return true;
-          }
+            if (set.has(word.toLowerCase())) {
+                return true;
+            }
         }
-      }
+    }
     return false;
 }
-async function fetchJsons(url) {
-    let x = await fetch(url);
-    x = await x.text();
-    return new Set(JSON.parse(decodeURIComponent(x)))
-}
-
 window.addEventListener('message', function (event) {
     let data = event.data;
     if (/inserted females/g.test(data[0])) {
-        females.add(data[1])
+        if (females.size) {
+            females.add(data[1])
+        }
+        testFset.add(data[1])
     }
     else if (/deleted from females/g.test(data[0])) {
         females.delete(data[1])
+        testFset.delete(data[1])
     }
     else if (/inserted males/g.test(data[0])) {
         males.add(data[1])
@@ -349,16 +352,15 @@ window.addEventListener('message', function (event) {
     if (males.size == 0) {
         if (data[0] == "females") {
             females = data[1];
+            testFset = structuredClone(data[1])
         }
         else if (data[0] == "males") {
             males = data[1];
             observer.observe(elTarget, { childList: true, subtree: true, attributes: false, characterData: false });
-            observer.isConnectd = true;
             observerr.observe(targetElement, { attributes: true });
-            observerr.isConnectd = true;
         }
     }
     else {
         console.log("no way");
     }
-});  
+});   
