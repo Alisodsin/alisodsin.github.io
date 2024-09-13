0e022e264069a14c0a1874a792d13f42e362c4a2
Alisodsin <leaflits@gmail.com>
2024-09-13 10:33:10 +0300

    ok

---
+++ java/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+       seprator.S.big();
+        System.out.println("hello");
+    }
+}
---
+++ java/seprator/Io.java
@@ -0,0 +1,9 @@
+package seprator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Io {
+    private static final InputStreamReader soso = new InputStreamReader(System.in);
+    public static final BufferedReader in = new BufferedReader(soso);
+}
