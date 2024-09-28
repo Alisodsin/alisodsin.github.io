b80dbd73e291cf00f2187bb2b6c0aa28f4e9f079
Alisodsin <leaflits@gmail.com>
2024-09-28 07:53:34 +0300

    ok

--- study/gui/java/src/Controller.java
+++ study/gui/java/src/Controller.java
@@ -1,3 +1,56 @@
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller {
 
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    //
+    private Stage window;
+    private Scene scene;
+    private Parent root;
+
+    public void changeScene(ActionEvent e) throws IOException {
+        System.out.println("this is changeScene method");
+        var button = (Button) e.getTarget();
+        scene = button.getScene();
+        window = (Stage) scene.getWindow();
+        if (button.getId().equals("button1")) {
+            root = FXMLLoader.load(getClass().getResource("resources/v2.fxml"));
+            scene.setRoot(root);
+            window.setScene(scene);
+        } else {
+            root = FXMLLoader.load(getClass().getResource("resources/v1.fxml"));
+            scene.setRoot(root);
+            window.setScene(scene);
+        }
+    }
+
+    public void anotherWayToChangeScene() throws IOException {
+        System.out.println("this is anotherWayToChangeScene method");
+        if (button1 != null) {
+            root = FXMLLoader.load(getClass().getResource("resources/v2.fxml"));
+            scene = button1.getScene();
+            window = (Stage) scene.getWindow();
+            scene.setRoot(root);
+            window.setScene(scene);
+
+        } else {
+            root = FXMLLoader.load(getClass().getResource("resources/v1.fxml"));
+            scene = button2.getScene();
+            window = (Stage) scene.getWindow();
+            scene.setRoot(root);
+            window.setScene(scene);
+        }
+    }
 }
