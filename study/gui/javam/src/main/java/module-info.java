933bc025515db41dfb57c93145a36c1e6d344bb0
Alisodsin <leaflits@gmail.com>
2024-09-28 22:15:12 +0300

    ok

--- study/gui/javam/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.java {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.java to javafx.fxml;
-    exports com.example.java;
-}
